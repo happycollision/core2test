c15d0f8f0ba5a52d6dabf9c72adc3b2e73d0a0f2
Don Denton <don@happycollision.com>
2017-11-06 16:26:53 -0600

    following https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-vsc

---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+namespace core2test.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsHappy { get; set; }
+    }
+}
---
+++ Models/PersonContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace core2test.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+
+    }
+}
